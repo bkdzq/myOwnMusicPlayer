5520bfbc45e98ea389ab65b2b301dc8c15a3bd57
qian ZHANG <53816482+bkdzq@users.noreply.github.com>
2022-11-24 19:20:33 +0100

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,62 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "大鱼海棠|giant fish",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "太阳照常升起|sun rise as usual",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "女儿情|the strong emothion of a queen",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "我的祖国|my country and me",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "时间都去哪了|where's the time? ",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "沧海一声笑|be relaxed near the sea",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "在水一方|love the girl who is cold to me",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-7",
+    src: "music-7"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
