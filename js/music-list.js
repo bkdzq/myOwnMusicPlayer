af8d0b9dea2fac59b96c1336ea1a110873c73300
qian ZHANG <53816482+bkdzq@users.noreply.github.com>
2022-12-17 20:15:41 +0100

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -52,6 +52,78 @@ let allMusic = [
     img: "music-1",
     src: "music-7"
   },
+  {
+    name: "但愿人长久|see the same moon despite staying apart",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-8"
+  },
+  {
+    name: "新贵妃醉酒|the nervous wife of king",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-9"
+  },
+   {
+    name: "飞雪玉花|snow fly as flower",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-10"
+  },
+  {
+    name: "亮剑|show your sword!",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-11"
+  },
+  {
+    name: "神话|A beautiful story continueing thousands of years",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-12"
+  },
+  {
+    name: "夜空中最亮的星|the star shining in sky",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-13"
+  },
+   {
+    name: "千与千寻|always with me",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-14"
+  },
+  {
+    name: "我和我的祖国|my country and me",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-15"
+  },
+  {
+    name: "莫斯科郊外的晚上|The night on moscow'soutskirts",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-16"
+  },
+  {
+    name: "喀秋莎|Катюша",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-17"
+  },
+  {
+    name: "田纳西华尔兹|the tennessee waltz",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-18"
+  },
+  {
+    name: "雄赳赳 气昂昂 跨过鸭绿江! | cross the Yalu River!",
+    artist: "中级错题家|medium level problem solver",
+    img: "music-1",
+    src: "music-19"
+  },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
